b8d981cd41f303b0f44d59b4063167391cd62673
dlo <isnatch90@gmail.com>
2022-11-09 19:19:28 +0900

    스웨거 적용

---
+++ src/main/java/com/example/boilerplate/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.example.boilerplate.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi(){
+        return GroupedOpenApi.builder()
+            .group("v1")
+            .pathsToMatch("/api/**")
+            .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+            .info(info());
+    }
+
+    @Bean
+    public Info info(){
+        return new Info()
+            .title("title")
+            .description("desc");
+    }
+}
